3dd5368706f4ec192ea4fdabdb753db46777b133
Ethan <ethanwilson.2000@gmail.com>
2017-11-29 19:16:39 +0000

    test

--- Example.py
+++ Example.py
@@ -8,9 +8,6 @@ if __name__ == '__main__':
     print("")
     
     
-    
-
-
 
 #    - - - - - - - - - -
 #pe1 1 0 0 0 0 0 0 0 0 0 
@@ -19,30 +16,43 @@ if __name__ == '__main__':
 #pe4 1 1 0 0 0 0 0 0 0 0
 
 
+#availability = {"name1": [0,0,0,0,0,0,0,0,0,0,]
+#                "name2": [0,0,0,0,0,0,0,0,0,0,]}
+
 class rota:
 
     def __init__(self,people):
 
         self.people = people #Saves the list of people as a self var.
         self.numPeople = len(people) #Creates a var for number of people in list.
-        self.list1 = [ [0] *8] *self.numPeople #Creates a martix of 0s for use in matching algo.
+        self.availability = [ [0] *8] *self.numPeople #Creates a martix of 0s for use in matching algo.
         
-        print('init done')
+        
+        self.dict = {key: None for key in self.people}
+        print(self.dict)
+        
+        
+        print('init ddddddone')
 
 
     def printAvailability(self):
+        '''
+        Loops through and prints:
+         - Everyone in list 'people'
+         - Their availability in list 'availability'
+        ''' 
         
         for x in range(self.numPeople):
             print('\n' + self.people[x], end="\t")
             
             for y in range(self.numPeople):
-                print(self.list1[x][y], end='\t')
+                print(self.availability[x][y], end='\t')
 
         print("\n")
         
     def editAvailability(self):
 
-        print("Select person to edit")
+        person = input("Select person to edit")
         
         for i in range(self.numPeople):
             print("{}. {}".format(i, self.people[i]))
@@ -54,6 +64,4 @@ class rota:
 rota1 = rota(["john", "lewis", "bob", "wayne"])
 rota1.printAvailability()
 rota1.editAvailability()
-print("nice")
-#nice
 
